643b1727f5f9a74869409fe5c6695f7ca430dcba
khoveyc@gmail.com <khoveyc@gmail.com>
2020-02-17 15:25:49 -0700

    Loading images on tooltips :)

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactMapGL, { Source, Layer } from 'react-map-gl';
+import ReactMapGL, { Source, Layer, PointerEvent } from 'react-map-gl';
 import { Feature, LineString } from 'geojson';
 import TrackData from '../data/route.json';
 import ImagePoints from '../builders/image_points';
@@ -21,8 +21,15 @@ interface Viewport {
   readonly zoom: number;
 }
 
+interface Location {
+  readonly x: number;
+  readonly y: number;
+}
+
 interface State {
   readonly viewport: Viewport;
+  readonly hoveredFeature: Feature | null;
+  readonly hoveredLocation: Location | null;
 }
 
 export default class extends React.Component<{}, State> {
@@ -33,11 +40,62 @@ export default class extends React.Component<{}, State> {
       latitude: 38.74433850188757,
       longitude: -109.56237695591732,
       zoom: 15,
-    }
+    },
+    hoveredFeature: null,
+    hoveredLocation: null,
   };
 
   setViewport = (viewport: Viewport): void => {
+    this.resetHover();
     this.setState({ viewport });
+  }
+
+  resetHover = (): void => {
+    this.setState({
+      hoveredFeature: null,
+      hoveredLocation: null,
+    });
+  }
+
+  onHover = ({ features, srcEvent }: PointerEvent): void => {
+    const { offsetX, offsetY } = srcEvent;
+
+    if (features === undefined) {
+      this.resetHover();
+
+      return;
+    };
+
+    const hoveredFeature = features.find(({ source }) => source === 'pictures');
+
+    if (hoveredFeature === undefined) {
+      this.resetHover();
+
+      return;
+    };
+
+    this.setState({
+      hoveredFeature,
+      hoveredLocation: {
+        x: offsetX,
+        y: offsetY,
+      },
+    });
+  }
+
+  get tooltip() {
+    const { hoveredFeature, hoveredLocation } = this.state;
+
+    if (hoveredFeature === null || hoveredLocation === null) return null;
+
+    const name = hoveredFeature.properties && hoveredFeature.properties.name;
+    const { x, y } = hoveredLocation;
+
+    return (
+      <div className="tooltip" style={{ top: y, left: x }}>
+        <img src={require(`../images/IMG_${name}.jpg`)} />
+      </div>
+    );
   }
 
   render() {
@@ -46,11 +104,12 @@ export default class extends React.Component<{}, State> {
         {...this.state.viewport}
         mapStyle="mapbox://styles/mapbox/satellite-v9"
         onViewportChange={this.setViewport}
+        onHover={this.onHover}
         mapboxApiAccessToken="pk.eyJ1IjoidXBlbCIsImEiOiJjajllZ29reTUyYTJoMndsc3ZtdGg2NXpsIn0.Y6sKlsUA9ZIm8rHfklQPaQ"
       >
-        <Source type="geojson" data={Data} lineMetrics>
+        <Source id='route' type="geojson" data={Data} lineMetrics>
           <Layer
-            id="route"
+            id="route-string"
             type="line"
             layout={{
               'line-join': 'round',
@@ -71,9 +130,9 @@ export default class extends React.Component<{}, State> {
             }}
           />
         </Source>
-        <Source type="geojson" data={ImagePoints}>
+        <Source id='pictures' type="geojson" data={ImagePoints}>
           <Layer
-            id="pictures"
+            id="pictures-markers"
             type="circle"
             paint={{
               'circle-radius': 6,
@@ -81,6 +140,7 @@ export default class extends React.Component<{}, State> {
             }}
           />
         </Source>
+        {this.tooltip}
       </ReactMapGL>
     );
   }
